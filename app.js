4929fe73b6d21d931105c5cbf93e0c6837b34ecf
Albert <ishengomaalbert@gmail.com>
2023-03-07 11:33:21 +0300

    Rendering Lists

--- app.js
+++ app.js
@@ -5,31 +5,62 @@ const app = Vue.createApp({
       title: "The Land of Wind",
       author: "Park jung ki",
       age: 23,
-      showBooks:true,
-      x:0,
-      y:0
+      showBooks: true,
+      x: 0,
+      y: 0,
+      movies: [
+        {
+          title: "The Matrix",
+          year: 1999,
+          genre: "Sci-Fi",
+          rating: 8.7,
+        },
+        {
+          title: "The Lion King",
+          year: 1994,
+          genre: "Animation",
+          rating: 8.5,
+        },
+        {
+          title: "The Godfather",
+          year: 1972,
+          genre: "Crime",
+          rating: 9.2,
+        },
+        {
+          title: "The Shawshank Redemption",
+          year: 1994,
+          genre: "Drama",
+          rating: 9.3,
+        },
+        {
+          title: "The Avengers",
+          year: 2012,
+          genre: "Action",
+          rating: 8.0,
+        },
+      ],
     };
   },
-  methods:{
-    changeTitle(title){
-      this.title = title
+  methods: {
+    changeTitle(title) {
+      this.title = title;
     },
-    toggle(){
-        this.showBooks = !this.showBooks
+    toggle() {
+      this.showBooks = !this.showBooks;
     },
 
-    handleEvent(e,data){
-      console.log('Event type: ',e.type);
+    handleEvent(e, data) {
+      console.log("Event type: ", e.type);
       if (data) {
-        console.log('Data: ',data);
+        console.log("Data: ", data);
       }
     },
-    handleMousemove(e){
+    handleMousemove(e) {
       this.x = e.offsetX;
       this.y = e.offsetY;
-
-    }
-  }
+    },
+  },
 });
 
 app.mount("#app");
